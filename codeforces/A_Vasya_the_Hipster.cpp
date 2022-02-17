cfd7b6d2637f8381565fa45d956e3db3f9699747
dabananda <dmitraofficial@gmail.com>
2022-02-17 17:01:33 +0600

    Codeforces 581A solved in C++

---
+++ codeforces/A_Vasya_the_Hipster.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << min(a, b) << " " << (max(a, b) - min(a, b)) / 2 << endl;
+
+  return 0;
+}
