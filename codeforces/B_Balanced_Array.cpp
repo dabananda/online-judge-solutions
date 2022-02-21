f103c89e0aa522fd7d3d660c333e0deb50f2706e
dabananda <dmitraofficial@gmail.com>
2022-02-21 21:43:47 +0600

    Codeforces 1343B solved in C++

---
+++ codeforces/B_Balanced_Array.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n; 
+    cin >> n;
+    if (n % 2 != 0) {
+      cout << "NO" << endl;
+    } else {
+      if ((n / 2) % 2 != 0) {
+        cout << "NO" << endl;
+      } else {
+        vector<int> even, odd;
+        int evenSum = 0, oddSum = 0, oddArr[n / 2], j = 0;
+        bool flag;
+        for (int i = 2; i <= n; i += 2) {
+          even.push_back(i);
+          evenSum += i;
+        }
+        for (int i = 1; i < n - 1; i += 2) {
+          oddArr[j] = i;
+          oddSum += i;
+          j++;
+        }
+        for (int i = n + 1; ; i += 2) {
+          if (oddSum + i > evenSum) {
+            flag = false;
+            break;
+          }
+          if (oddSum + i == evenSum) {
+            oddArr[j] = i;
+            flag = true;
+            break;
+          }
+        }
+        if (flag) {
+          cout << "YES" << endl;
+          for (int i = 0; i < n / 2; i++) {
+            cout << even[i] << " ";
+          }
+          for (int i = 0; i < n / 2; i++) {
+            cout << oddArr[i] << " ";
+          }
+          cout << endl;
+        } else {
+          cout << "NO" << endl;
+        }
+      }
+    }
+  }
+
+  return 0;
+}
