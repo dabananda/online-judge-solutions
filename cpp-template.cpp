b7874448ee606fa55953416f7585580768cda8c6
Dabananda Mitra <dmitraofficial@gmail.com>
2022-09-02 12:04:49 +0600

    Basic Cpp template

---
+++ cpp-template.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+
+typedef long long int lli;
+
+using namespace std;
+
+int main() {
+  lli testCase;
+  cin >> testCase;
+  while (testCase--) {
+
+  }
+
+  return 0;
+}
