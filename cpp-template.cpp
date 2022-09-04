fe0a34a18a128a3495f3d6964bb9e477ed386b23
Dabananda Mitra <dmitraofficial@gmail.com>
2022-09-04 09:13:08 +0600

    Updated with fast i/o

--- cpp-template.cpp
+++ cpp-template.cpp
@@ -6,9 +6,12 @@
 
 typedef long long int lli;
 
+#define TLE ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
 using namespace std;
 
 int main() {
+  TLE;
   lli testCase;
   cin >> testCase;
   while (testCase--) {
