9e2938a1fd5b7010735b527f3a9146e8a3dfb557
Dabananda Mitra <imdmitra@gmail.com>
2023-09-22 12:11:15 +0600

    Codeforces Round 898 (Div. 4)

---
+++ codeforces/C_Target_Practice.cpp
@@ -0,0 +1,55 @@
+// Dabananda Mitra, CSE (2019-20), ISTT.
+// Email: imdmitra@outlook.com
+// LinkedIn: https://www.linkedin.com/in/dabanandamitra/
+// GitHub: https://github.com/dabananda
+// Portfolio: https://dmitra.netlify.app/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pi pair<int, int>
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define rloop(i, a, b) for (int i = b; i >= a; i--)
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+int score[10][10] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+    {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+    {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+    {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+void solution() {
+  // start code from here
+  char c[10][10];
+  int sum = 0;
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      cin >> c[i][j];
+      if (c[i][j] == 'X') {
+        sum += score[i][j];
+      }
+    }
+  }
+  cout << sum << "\n";
+}
+
+int main() {
+  fastio;
+  int testCase;
+  cin >> testCase;
+  while (testCase--) {
+    solution();
+  }
+  cout << "\n";
+  return 0;
+}
