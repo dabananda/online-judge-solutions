8d69fc7c4cd8c36becd1f936440f9464951569db
Dabananda Mitra <imdmitra@gmail.com>
2023-09-26 10:11:37 +0600

    Educational Codeforces Round 109

--- codeforces/B_Permutation_Sort.cpp
+++ codeforces/B_Permutation_Sort.cpp
@@ -27,7 +27,7 @@ void solution() {
   int ans = 2;
   if (is_sorted(v.begin(), v.end())) {
     ans = 0;
-  } else if (v[0] == 1 && v[n - 1] == n) {
+  } else if (v[0] == 1 || v[n - 1] == n) {
     ans = 1;
   } else if (v[0] == n && v[n - 1] == 1) {
     ans = 3;
