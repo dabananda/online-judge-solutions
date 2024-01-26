057eb458615c9be5ac65b3513abac9c2fb741686
dabananda <imdmitra@gmail.com>
2024-01-26 12:25:19 +0600

    143 ms - 96.62%, 116.67 MB - 64.09%

--- leetcode/234. Palindrome Linked List.cpp
+++ leetcode/234. Palindrome Linked List.cpp
@@ -1,28 +1,32 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode *curr = head;
-        vector<int> v;
-        while (curr) {
-            v.push_back(curr->val);
-            curr = curr->next;
+        // finding middle
+        ListNode *fast = head, *slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
         }
-        int len = v.size();
-        int last = len - 1;
-        for (int i = 0; i < len / 2; i++, last--) {
-            if (v[i] != v[last]) {
+        // If the list has an odd length, move slow one step ahead
+        if (fast) {
+            slow = slow->next;
+        }
+        // reversing list (slow)
+        ListNode *prev = nullptr, *temp = nullptr;
+        while (slow) {
+            temp = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = temp;
+        }
+        // comparing the two halves
+        ListNode *left = head, *right = prev;
+        while (right) {
+            if (left->val != right->val) {
                 return false;
             }
+            left = left->next;
+            right = right->next;
         }
         return true;
     }
