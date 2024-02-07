2a2d78db4f467e9c71b0c78d80189d169619f4c9
dabananda <imdmitra@gmail.com>
2024-02-07 23:51:35 +0600

    Time: O(n), Space: O(1), Runtime: 4 ms - 85.26%, Memory: 17.68 MB - 15.19%

---
+++ leetcode/2011. Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,17 @@
+// Link: https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int res = 0;
+        int size = operations.size();
+        for (int i = 0; i < size; i++) {
+            if (operations[i] == "X++" || operations[i] == "++X") {
+                res++;
+            } else {
+                res--;
+            }
+        }
+        return res;
+    }
+};
