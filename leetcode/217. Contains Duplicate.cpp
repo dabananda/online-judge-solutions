d64f2c88deb14faa41e74eff66f162c418d54cfb
dabananda <imdmitra@gmail.com>
2024-02-20 22:35:26 +0600

    95 ms, 60.85 MB

--- leetcode/217. Contains Duplicate.cpp
+++ leetcode/217. Contains Duplicate.cpp
@@ -1,13 +1,10 @@
 class Solution {
- public:
-  bool containsDuplicate(vector<int>& nums) {
-    int size = nums.size();
-    sort(nums.begin(), nums.end());
-    for (int i = 1; i < size; i++) {
-      if (nums[i] == nums[i - 1]) {
-        return true;
-      }
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) return true;
+        }
+        return false;
     }
-    return false;
-  }
 };
