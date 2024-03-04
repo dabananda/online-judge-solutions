27e98f8abf1421b7cb64960a17349ae1825c145c
dabananda <imdmitra@gmail.com>
2024-03-04 15:23:46 +0600

    Updated

--- codeforces/AuthFile.cpp
+++ codeforces/AuthFile.cpp
@@ -3,54 +3,57 @@ Author: Dabananda Mitra
 Email: imdmitra@gmail.com
 GitHub: https://github.com/dabananda
 LinkedIn: https://www.linkedin.com/in/dabanandamitra/
-Education: Computer Science and Engineering (CSE), Session: 2019-2020, Institute of Science Trade & Technology (ISTT)
-Time & Date: 2024-03-04 15:09:25 (Dhaka, Bangladesh)
+Department of Computer Science and Engineering (CSE), Session: 2019-2020, Institute of Science Trade & Technology (ISTT)
+Time & Date: 2024-03-04 03:23:31 PM (Dhaka, Bangladesh)
 */
+
 #include <ctime>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 
-std::string getCurrentDateTime() {
+using namespace std;
+
+string getCurrentDateTime() {
   time_t now = time(0);
   struct tm* localTime = localtime(&now);
 
   char buffer[80];
-  strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", localTime);
+  strftime(buffer, sizeof(buffer), "%Y-%m-%d %I:%M:%S %p", localTime);
 
   return buffer;
 }
 
 int main() {
-  std::string fileName;\
-  std::string authorInfo =
+  string filePath;
+  string authorInfo =
       "/*\n"
       "Author: Dabananda Mitra\n"
       "Email: imdmitra@gmail.com\n"
       "GitHub: https://github.com/dabananda\n"
       "LinkedIn: https://www.linkedin.com/in/dabanandamitra/\n"
-      "Education: Computer Science and Engineering (CSE), Session: 2019-2020, Institute of Science Trade & Technology (ISTT)\n"
+      "Department of Computer Science and Engineering (CSE), Session: 2019-2020, Institute of Science Trade & Technology (ISTT)\n"
       "Time & Date: " +
       getCurrentDateTime() +
       " (Dhaka, Bangladesh)\n"
-      "*/\n";
+      "*/\n\n";
 
-  // Input file name
-  std::cout << "Enter the file name: ";
-  std::getline(std::cin, fileName);
+  // Input file path
+  cout << "Enter the file path: ";
+  getline(cin, filePath);
 
   // Open the file
-  std::ifstream inputFile(fileName);
+  ifstream inputFile(filePath);
   if (!inputFile.is_open()) {
-    std::cerr << "Error opening file: " << fileName << std::endl;
+    cerr << "Error opening file: " << filePath << endl;
     return 1;
   }
 
   // Read the content of the file
-  std::stringstream buffer;
+  stringstream buffer;
   buffer << inputFile.rdbuf();
-  std::string fileContent = buffer.str();
+  string fileContent = buffer.str();
 
   // Add author information at the beginning
   fileContent = authorInfo + fileContent;
@@ -59,9 +62,9 @@ int main() {
   inputFile.close();
 
   // Open the file for writing
-  std::ofstream outputFile(fileName);
+  ofstream outputFile(filePath);
   if (!outputFile.is_open()) {
-    std::cerr << "Error opening file for writing: " << fileName << std::endl;
+    cerr << "Error opening file for writing: " << filePath << endl;
     return 1;
   }
 
@@ -72,14 +75,14 @@ int main() {
   outputFile.close();
 
   // Commit and push to GitHub
-  std::string commitMessage;
-  std::cout << "Enter the commit message: ";
-  std::getline(std::cin, commitMessage);
+  string commitMessage;
+  cout << "Enter the commit message: ";
+  getline(cin, commitMessage);
 
-  std::string command = "git add " + fileName + " && git commit -m \"" + commitMessage + "\" && git push";
+  string command = "git add " + filePath + " && git commit -m \"" + commitMessage + "\" && git push";
   system(command.c_str());
 
-  std::cout << "Changes committed and pushed to GitHub successfully." << std::endl;
+  cout << "Changes committed and pushed to GitHub successfully." << endl;
 
   return 0;
 }
