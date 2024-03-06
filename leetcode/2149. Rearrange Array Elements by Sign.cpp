8067cc63bfad592b7eabe6b419b9216b39224072
dabananda <imdmitra@gmail.com>
2024-03-06 23:21:09 +0600

    151 ms, 137.10 MB

---
+++ leetcode/2149. Rearrange Array Elements by Sign.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> pos;
+        vector<int> neg;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0)
+                pos.push_back(nums[i]);
+            else
+                neg.push_back(nums[i]);
+        }
+        for (int i = 0; i < n / 2; i++) {
+            nums[2 * i] = pos[i];
+            nums[2 * i + 1] = neg[i];
+        }
+        return nums;
+    }
+};
