1020551731e3cec6fe0f4bd2a58e8057fa58612b
dabananda <imdmitra@gmail.com>
2024-03-07 09:28:34 +0600

    147 ms, 130.53 MB

--- leetcode/2149. Rearrange Array Elements by Sign.cpp
+++ leetcode/2149. Rearrange Array Elements by Sign.cpp
@@ -1,19 +1,31 @@
+/*
+Author: Dabananda Mitra
+Email: imdmitra@gmail.com
+GitHub: https://github.com/dabananda
+LinkedIn: https://www.linkedin.com/in/dabanandamitra/
+Department of Computer Science and Engineering (CSE), Session: 2019-2020, Institute of Science Trade & Technology (ISTT)
+*/
+
+// Problem link: https://leetcode.com/problems/rearrange-array-elements-by-sign/
+// Time complexity: O(n)
+// Space complexity: O(n)
 class Solution {
-public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> pos;
-        vector<int> neg;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0)
-                pos.push_back(nums[i]);
-            else
-                neg.push_back(nums[i]);
-        }
-        for (int i = 0; i < n / 2; i++) {
-            nums[2 * i] = pos[i];
-            nums[2 * i + 1] = neg[i];
-        }
-        return nums;
+ public:
+  vector<int> rearrangeArray(vector<int>& nums) {
+    int n = nums.size();
+    // All positive numbers will be in even position and negetive will be in odd position
+    int pos = 0, neg = 1;
+    vector<int> temp(n, 0);
+    for (int i = 0; i < n; i++) {
+      if (nums[i] < 0) {
+        temp[neg] = nums[i];
+        neg += 2;
+      } else {
+        temp[pos] = nums[i];
+        pos += 2;
+      }
     }
+    nums = temp;
+    return nums;
+  }
 };
