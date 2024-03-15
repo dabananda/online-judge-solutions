926b418d9458eec244e8e8e73bcfdfc6c08e7a57
dabananda <imdmitra@gmail.com>
2024-03-15 17:43:02 +0600

    Day 58

---
+++ leetcode/88. Merge Sorted Array.cpp
@@ -0,0 +1,25 @@
+/*
+Author: Dabananda Mitra
+Email: imdmitra@gmail.com
+GitHub: https://github.com/dabananda
+LinkedIn: https://www.linkedin.com/in/dabanandamitra/
+Department of Computer Science and Engineering (CSE), Session: 2019-2020, Institute of Science Trade & Technology (ISTT)
+*/
+
+// Problem link: https://leetcode.com/problems/merge-sorted-array/description/
+// Time complexity: O(m + n)
+// Space complexity: O(1)
+
+class Solution {
+ public:
+  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = m - 1, j = n - 1, k = m + n - 1;
+    while (j >= 0) {
+      if (i >= 0 && nums1[i] > nums2[j]) {
+        nums1[k--] = nums1[i--];
+      } else {
+        nums1[k--] = nums2[j--];
+      }
+    }
+  }
+};
