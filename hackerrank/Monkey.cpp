ad6f10084d0541bd8bb35d12510bce7fe7734ccb
dabananda <imdmitra@gmail.com>
2024-05-29 12:22:36 +0600

    Mid Term | Introduction to C++ for DSA | Batch 05

--- hackerrank/Monkey.cpp
+++ hackerrank/Monkey.cpp
@@ -2,23 +2,26 @@
 
 using namespace std;
 
-string trim(string s) {
-  int n = s.size();
-  string newString = "";
+void trim(char s[], char newString[], int n) {
+  int j = 0;
   for (int i = 0; i < n; i++) {
     if (s[i] != ' ') {
-      newString += s[i];
+      newString[j] = s[i];
+      j++;
     }
   }
-  return newString;
+  newString[j] = '\0';
 }
 
 int main() {
-  string s;
-  while (getline(cin, s)) {
-    s = trim(s);
-    sort(s.begin(), s.end());
-    cout << s << endl;
+  char s[100001];
+  while (cin.getline(s, 100001)) {
+    int n = strlen(s);
+    char newString[n + 1];
+    trim(s, newString, n);
+    n = strlen(newString);
+    sort(newString, newString + n);
+    cout << newString << endl;
   }
 
   return 0;
--- hackerrank/Who_Is_It.cpp
+++ hackerrank/Who_Is_It.cpp
@@ -4,29 +4,40 @@ using namespace std;
 
 class Student {
  public:
-  int id;
-  string name;
-  char section;
-  int marks;
+  int id, marks;
+  char name[101], section;
+
+  Student(int id, char name[], char section, int marks) {
+    this->id = id;
+    strcpy(this->name, name);
+    this->section = section;
+    this->marks = marks;
+  }
 };
 
 int main() {
   int t;
   cin >> t;
   while (t--) {
-    Student s[3];
-    for (int i = 0; i < 3; i++) {
-      cin >> s[i].id >> s[i].name >> s[i].section >> s[i].marks;
-    }
-    int idx = 0;
-    for (int i = 1; i < 3; i++) {
-      if (s[i].marks > s[idx].marks ||
-          (s[i].marks == s[idx].marks && s[i].id < s[idx].id)) {
-        idx = i;
-      }
-    }
-    cout << s[idx].id << " " << s[idx].name << " " << s[idx].section << " " << s[idx].marks << endl;
-  }
+    int id, marks;
+    char name[101], section;
 
+    cin >> id >> name >> section >> marks;
+    Student s1(id, name, section, marks);
+    cin >> id >> name >> section >> marks;
+    Student s2(id, name, section, marks);
+    cin >> id >> name >> section >> marks;
+    Student s3(id, name, section, marks);
+
+    Student topper = s1;
+    if (s2.marks > topper.marks || (s2.marks == topper.marks && s2.id < topper.id)) {
+     topper = s2;
+    }
+    if (s3.marks > topper.marks || (s3.marks == topper.marks && s3.id < topper.id)) {
+     topper = s3;
+    }
+
+    cout << topper.id << " " << topper.name << " " << topper.section << " " << topper.marks << endl;
+  }
   return 0;
 }
