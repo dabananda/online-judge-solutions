75b6dcb358cd725a50c0087854206ee59d15176f
dabananda <imdmitra@gmail.com>
2024-09-24 23:36:03 +0600

    Graph

---
+++ leetcode/207. Course Schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        for (auto it : prerequisites) {
+            adj[it[1]].push_back(it[0]);
+        }
+        
+        int indegree[5000] = {0};
+        for (int i = 0; i < numCourses; i++) {
+            for (auto it : adj[i]) {
+                indegree[it]++;
+            }
+        }
+        
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        
+        vector<int> topo;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+        
+        return topo.size() == numCourses;
+    }
+};
