c8a91e22aac1f4c677999691b34d738a1f0cfa40
dabananda <imdmitra@gmail.com>
2024-09-27 12:44:07 +0600

    MST

---
+++ cses/Road_Reparation.cpp
@@ -0,0 +1,94 @@
+/**
+ * Link: https://cses.fi/problemset/task/1675/
+ * Author: Dabananda Mitra
+ * Email: imdmitra@gmail.com
+ * Created: 2024-09-27 12:14:19
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e5 + 5;
+int leader[N];
+int groupSize[N];
+
+class Edge {
+ public:
+  int u, v, w;
+  Edge(int u, int v, int w) {
+    this->u = u;
+    this->v = v;
+    this->w = w;
+  }
+};
+
+void dsuInit(int n) {
+  for (int i = 1; i <= n; i++) {
+    leader[i] = -1;
+    groupSize[i] = 1;
+  }
+}
+
+int getLeader(int node) {
+  if (leader[node] == -1) {
+    return node;
+  }
+  int l = getLeader(leader[node]);
+  leader[node] = l;
+  return l;
+}
+
+void makeUnion(int x, int y) {
+  int a = getLeader(x);
+  int b = getLeader(y);
+  if (groupSize[a] > groupSize[b]) {
+    leader[b] = a;
+    groupSize[a] += groupSize[b];
+  } else {
+    leader[a] = b;
+    groupSize[b] += groupSize[a];
+  }
+}
+
+bool comp(Edge a, Edge b) {
+  return a.w < b.w;
+}
+
+int main() {
+  int n, e;
+  cin >> n >> e;
+  vector<Edge> edgeList;
+  while (e--) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    edgeList.push_back(Edge(u, v, w));
+  }
+  sort(edgeList.begin(), edgeList.end(), comp);
+  long long int cost = 0;
+  dsuInit(n);
+  for (Edge ed : edgeList) {
+    int a = getLeader(ed.u);
+    int b = getLeader(ed.v);
+    if (a != b) {
+      makeUnion(a, b);
+      cost += ed.w;
+    }
+  }
+  int mainLeader = getLeader(1);
+  int flag = true;
+  for (int i = 2; i <= n; i++) {
+    if (getLeader(i) != mainLeader) {
+      flag = false;
+      break;
+    }
+  }
+  if (flag) {
+    cout << cost << endl;
+  } else {
+    cout << "IMPOSSIBLE" << endl;
+  }
+
+  return 0;
+}
