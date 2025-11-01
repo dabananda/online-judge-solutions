49cc46ee1dd98d6cfa5147c11d3042c7b2847eee
dabananda <dabananda.dev@gmail.com>
2025-11-01 11:46:02 +0600

    neet code - arrays & hashing

---
+++ leetcode/1. Two Sum.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var d = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var diff = target - nums[i];
+            if (d.ContainsKey(diff))
+            {
+                return new int[] {i, d[diff]};
+            }
+            d[nums[i]] = i;
+        }
+        return new int[] {};
+    }
+}
