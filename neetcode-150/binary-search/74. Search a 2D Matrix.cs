03bbbe9d41534a1e803d226a9d2822755056ad6e
dabananda <dabananda.dev@gmail.com>
2025-11-22 12:32:20 +0600

    neetcode 150 - binary search

---
+++ neetcode-150/binary-search/74. Search a 2D Matrix.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        int n = matrix.Length;
+        int m = matrix[0].Length;
+        int low = 0, high = n * m - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int row = mid / m, col = mid % m;
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
