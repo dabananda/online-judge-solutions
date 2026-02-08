d3277265c0c6d4a56544075dccb402b9b61c7ea7
dabananda <dabananda.dev@gmail.com>
2026-02-08 16:49:48 +0600

    dsa series - trees

--- dsa/tree.cpp
+++ dsa/tree.cpp
@@ -153,6 +153,42 @@ bool hasPathSum(TreeNode* root, int targetSum) {
   return hasPathSum(root->left, remaining) || hasPathSum(root->right, remaining);
 }
 
+// Delete node
+TreeNode* findMin(TreeNode* node) {
+  while (node->left) node = node->left;
+  return node;
+}
+
+TreeNode* deleteNode(TreeNode* root, int key) {
+  if (root == NULL) return root;
+  if (key < root->val)
+    root->left = deleteNode(root->left, key);
+  else if (key > root->val)
+    root->right = deleteNode(root->right, key);
+  else {
+    // no child
+    if (root->left == NULL && root->right == NULL) {
+      delete (root);
+      return NULL;
+    }
+    // one child
+    else if (root->left == NULL) {
+      TreeNode* temp = root->right;
+      delete root;
+      return temp;
+    } else if (root->right == NULL) {
+      TreeNode* temp = root->left;
+      delete root;
+      return temp;
+    }
+    // two child
+    TreeNode* temp = findMin(root->right);
+    root->val = temp->val;
+    root->right = deleteNode(root->right, temp->val);
+  }
+  return root;
+}
+
 // main function
 int main() {
   TreeNode* root = NULL;
@@ -190,6 +226,8 @@ int main() {
   cout << "Diameter: " << getDiameter(root) << endl;
   cout << "Is Balanced: " << isBalanced(root) << endl;
   cout << "Has Path Sum: " << hasPathSum(root, 10) << endl;
+  TreeNode* node = deleteNode(root, 10);
+  cout << "Delete Node: " << node->val << endl;
 
   return 0;
 }
